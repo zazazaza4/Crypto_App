d4f1d03cd45002e7aa279dbf75644ece11bfd737
Yaroslav <6715414@gmail.com>
2023-05-30 15:00:54 +0300

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 
+import { Header } from "components";
+
 import "./App.scss";
 import "swiper/swiper.min.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </>
   );
 }
