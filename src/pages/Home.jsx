c2b20ee9d1530989cc46c5643aa30b453147adc5
Yaroslav <6715414@gmail.com>
2023-06-01 14:27:12 +0300

    added Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import { CryptoSlide } from "components";
+
+const Home = () => {
+  return (
+    <>
+      <CryptoSlide />
+      <div className="container"></div>
+    </>
+  );
+};
+
+export default Home;
