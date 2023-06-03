58d414da3e68ce9d83543fd3b2ca9e8c325100bf
Yaroslav <6715414@gmail.com>
2023-06-03 16:55:26 +0300

    added sections to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
-import { CryptoSlide } from "components";
+import { CryptoSlide, OutlineButton, CryptoList } from "components";
+import { useHomeStore } from "stores";
 
 const Home = () => {
+  const store = useHomeStore();
+
   return (
     <>
       <CryptoSlide />
-      <div className="container"></div>
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Coins List</h2>
+            <Link to="/coins">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <CryptoList category={"coins"} />
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Exchanges List</h2>
+            <Link to="/exchanges">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <CryptoList category={"coins"} />
+        </div>
+      </div>
     </>
   );
 };
