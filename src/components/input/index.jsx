b5ab0ef32c2124f8b229bca473a3e387223fd5bc
Yaroslav <6715414@gmail.com>
2023-06-05 11:58:05 +0300

    added input component

---
+++ src/components/input/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+import "./input.scss";
+
+export const Input = ({ type, placeholder, value, onChange }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange ? (e) => onChange(e.target.value) : null}
+    />
+  );
+};
+
+Input.propTypes = {
+  type: PropTypes.oneOf(["text", "number", "search"]),
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
