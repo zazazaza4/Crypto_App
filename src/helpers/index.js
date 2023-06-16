1e35cbd8b55082ce9f2da413076f98c1157fb84e
Yaroslav <6715414@gmail.com>
2023-06-16 09:59:00 +0300

    added function formatNumber and const category

---
+++ src/helpers/index.js
@@ -0,0 +1,30 @@
+export function formatNumber(number) {
+  const exponent = number.toExponential().split("e")[1];
+  if (exponent < 0) {
+    const numberFixed = -exponent + 4;
+    number = number.toFixed(numberFixed);
+  }
+
+  number = String(number);
+
+  const parts = number.split(".");
+
+  if (parts.length !== 2) {
+    return number;
+  }
+
+  const integerPart = parts[0];
+  let decimalPart = parts[1];
+
+  if (decimalPart.length > 2) {
+    decimalPart =
+      decimalPart.slice(0, 2) + "..." + decimalPart.replaceAll(/000/g, "");
+  }
+
+  return integerPart + "." + decimalPart;
+}
+
+export const category = {
+  exchanges: "exchanges",
+  coins: "coins",
+};
