67fb1f2d9a165176db30ceb68a50e4c9f95b955b
Yaroslav <6715414@gmail.com>
2023-06-16 10:07:44 +0300

    changed string categories to const

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { CryptoSlide, OutlineButton, CryptoCarousel } from "components";
 import { useHomeStore } from "stores";
+import { category } from "helpers";
 
 const Home = () => {
   const store = useHomeStore();
@@ -12,21 +13,21 @@ const Home = () => {
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Coins List</h2>
-            <Link to="/coins">
+            <Link to={`/${category.coins}`}>
               <OutlineButton className="small">View more</OutlineButton>
             </Link>
           </div>
-          <CryptoCarousel category={"coins"} />
+          <CryptoCarousel category={category.coins} />
         </div>
 
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Exchanges List</h2>
-            <Link to="/exchanges">
+            <Link to={`/${category.exchanges}`}>
               <OutlineButton className="small">View more</OutlineButton>
             </Link>
           </div>
-          <CryptoCarousel category={"coins"} />
+          <CryptoCarousel category={category.exchanges} />
         </div>
       </div>
     </>
