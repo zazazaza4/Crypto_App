7b52d8c530b0e21a1e0f1344615f8d2344df8907
Yaroslav <6715414@gmail.com>
2023-08-13 23:20:50 +0300

    changed apiRoutesEnum

--- src/utils/enums/api/api.enum.js
+++ src/utils/enums/api/api.enum.js
@@ -1,14 +1,12 @@
 export const apiRoutesEnum = {
   EXCHANGES: {
     LIST: "/exchanges/list",
-    TICKERS: "/exchanges/list/:id/tickers",
-    VOLUME_CHART: "/exchanges/list/:id/volume_chart",
-    DETAILS: "/exchanges/list/:id",
+    DETAILS: "/exchanges/list",
   },
   COINS: {
     LIST: "/coins/list",
     SEARCH: "/coins/search",
     TRENDING: "/coins/trending",
-    DETAILS: "/coins/list/:id",
+    DETAILS: "/coins/list", // /coins/list/:id
   },
 };
