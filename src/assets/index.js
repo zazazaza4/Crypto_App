8ddb1149f9c29bf5c770d893ef6e392cb9ddcb34
Yaroslav <6715414@gmail.com>
2023-08-16 20:57:58 +0300

    export image for 404

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import logo from "./tmovie.png";
 import bg from "./footer-bg.jpg";
 import btcImg from "./bitcoin.webp";
+import image404 from "./404-image.jpg";
 
-export { logo, bg, btcImg };
+export { logo, bg, btcImg, image404 };
